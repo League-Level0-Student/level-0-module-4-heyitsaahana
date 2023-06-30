5888d91947665300c4323e033be088091c42ee85
league <league@imac-8.attlocal.net>
2023-06-30 16:58:37 -0700

    string,char

--- src/_03_char/_2_string_methods/StringMethods.java
+++ src/_03_char/_2_string_methods/StringMethods.java
@@ -5,24 +5,41 @@ public class StringMethods {
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it to contain a minimum of 3 characters
-
+		String y="hata";
+		
+		
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
-
+	System.out.println(  y.charAt(2) );	
 		
 		// 3. Print the length of your String to the console.
 		//    HINT: .length()
-
+ System.out.println(y.length());
 		
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
-
+ 	for(int x=0;x<y.length();x++) {
+ 		System.out.println(y.charAt(x)    );
+ 		
+ 		
+ 		
+ 		
+ 	}
 		
 		// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
 		//    Print the char's position to the console.
 		//    EXAMPLE: if your string is "abc" and you are searching 
 		//             for char 'b', then print "b is at index 1"
+ 		for( int m=0;m<y.length();m++        ) {
+ 	
+ 			
+ 			if(y.charAt(m )=='a' ) {
+ 				System.out.println(m);
+ 			}
+ 		}
+ 	
+ 	
 		
 	}
 }
